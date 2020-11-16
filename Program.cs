a08781872f9a835abf3fe6b966ded6281d9e0cd6
RichardLucas12 <richardlucas.om12@gmail.com>
2020-11-16 15:36:50 -0300

    pronto

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a operação \n soma subtração divisão ou multiplicação ");
+            string operacao = Console.ReadLine();
+
+            Console.WriteLine("Digite um numero");
+            float num1 = float.Parse( Console.ReadLine());
+            
+            Console.WriteLine("Digite outro");
+            float num2 = float.Parse( Console.ReadLine());
+
+            float result = 0 ;
+
+            switch(operacao){
+                case  "soma" :
+                    result = num1+num2;
+                    break;
+
+                case "subtracao" :
+                    result = num1-num2;
+                    break;
+
+                case "divisao" :
+                    result = num1/num2;
+                    break;
+
+                case "multiplicacao" : 
+                    result = num1*num2;
+                    break;
+            
+
+                default:
+                    Console.WriteLine("operacao invalida"); 
+                    break;
+                    
+            }
+            
+        }
+    }
+}
